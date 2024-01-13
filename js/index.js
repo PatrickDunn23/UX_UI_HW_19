2a96667f129f0341d2ba0bc6d1b2832f51f208c6
PatrickDunn23 <142137592+PatrickDunn23@users.noreply.github.com>
2024-01-13 17:59:33 +1000

    very  not working

--- js/index.js
+++ js/index.js
@@ -1,16 +1,36 @@
-console.log("your index.js file is loaded correctly");
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
 
-$("resumeButton").on("click", function(){
-    $(".resumeButton").toggleClass("active");
-    if ($(".resumeButton").hasClass("active")) {
-      $(".resumeBackground").css("height", "100vh");
-      $("#resumeImage").css("opacity","1");
-      $(".button").removeClass("fa-search").addClass("fa-times");
-    }
-    else {
-      $(".resumeBackground").css('height', "0vh");
-      $("#resumeImage").css("opacity","0");
-      $(".button").removeClass("fa-times").addClass("fa-search");
-    }
-  });
-  
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
